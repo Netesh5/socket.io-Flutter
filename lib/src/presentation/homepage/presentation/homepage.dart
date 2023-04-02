e9464995541ddd5c639fe5a076466f680e87b912
Netesh5 <netesh.paudel@gmail.com>
2023-04-02 22:30:39 +0545

    fetching data

--- lib/src/presentation/homepage/presentation/homepage.dart
+++ lib/src/presentation/homepage/presentation/homepage.dart
@@ -1,8 +1,9 @@
 import 'package:chatapp/core/theme_manager/colors.dart';
+import 'package:chatapp/src/presentation/homepage/bloc/username_cubit.dart';
 
 import 'package:chatapp/widgets/customdialog.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -40,7 +41,24 @@ class _HomeScreenState extends State<HomeScreen> {
                   size: 35,
                 ))),
         body: Center(
-          child: Text("Welcome, $username"),
+          child: BlocConsumer<UsernameCubit, Usernamestate>(
+            listener: (context, state) {
+              if (username.isNotEmpty) {
+                BlocProvider.of<UsernameCubit>(context).containValue();
+              } else if (username.isEmpty) {
+                BlocProvider.of<UsernameCubit>(context).novalue();
+              }
+            },
+            builder: (context, state) {
+              if (state == Usernamestate.hasvalue) {
+                return Text("Welcome, $username");
+              } else if (state == Usernamestate.novalue) {
+                return const SizedBox();
+              } else {
+                return const SizedBox();
+              }
+            },
+          ),
         ));
   }
 }
